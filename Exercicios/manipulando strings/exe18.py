03f73000f9d8a3c3808ca034f6b365838dc87d6a
Ruann-Pablo <ruannprime@gmial.com>
2025-12-24 16:59:53 -0300

    Refactor second name retrieval in string manipulation exercise for clarity

--- Exercicios/manipulando strings/exe18.py
+++ Exercicios/manipulando strings/exe18.py
@@ -1,4 +1,4 @@
 nome = str(input('Digite seu nome: ')).strip().title().split()
 
 print(f'Seu primeiro nome: {nome[0]}')
-print(f'Seu segundo nome: {nome[len(nome)-1]}')
+print(f'Seu segundo nome: {nome[-1]}')
