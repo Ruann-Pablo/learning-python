6a0745ed55b54ea56b2c9d905299dd3199ecee4f
Ruann-Pablo <ruannprime@gmial.com>
2026-01-10 00:49:20 -0300

    Add tuple examples for food and car items with print statements

---
+++ Exercicios/tupla/exe_tupla.py
@@ -0,0 +1,8 @@
+lanche = (('Hambúrguer', 'Suco'), ('Pizza', 'Pudim'))
+print(lanche)
+print(lanche[1])
+print(lanche[1][1])
+
+carro = ('Celta', 'Golf', 'Corolla', 'HB20')
+print(carro[1])
+print(carro[1][1])
