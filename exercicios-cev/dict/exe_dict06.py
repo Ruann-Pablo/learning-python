1e808d32d8b8b37dfbecb9d070b62bb8574f11f0
Ruann-Pablo <ruannprime@gmail.com>
2026-01-24 03:23:36 -0300

    Refactor input prompts and formatting for improved readability

--- exercicios-cev/dict/exe_dict06.py
+++ exercicios-cev/dict/exe_dict06.py
@@ -4,11 +4,17 @@ jogador = {}
 
 while True:
     soma_gols = 0
-    jogador['nome'] = str(input('Nome do jogador: ')).strip().title()
-    quant_partidas = int(input(f'Informe a quantidade de partidas jogadas por {jogador["nome"]}: '))
+    jogador["nome"] = str(input("Nome do jogador: ")).strip().title()
+    quant_partidas = int(
+        input(f'Informe a quantidade de partidas jogadas por {jogador["nome"]}: ')
+    )
 
     for i in range(quant_partidas):
-        quant_gols = int(input(f' Informe a quantidade de GOLS feitos por {jogador["nome"]} na {i + 1}ª partida: '))
+        quant_gols = int(
+            input(
+                f' Informe a quantidade de GOLS feitos por {jogador["nome"]} na {i + 1}ª partida: '
+            )
+        )
         lista_gols.append(quant_gols)
 
     jogador["gols"] = lista_gols[:]
@@ -19,31 +25,35 @@ while True:
     jogador["total_gols"] = soma_gols
 
     while True:
-        resposta = input('Quer continuar? [S/N]: ').lower().strip()
-        if resposta in ('s', 'n'):
+        resposta = input("Quer continuar? [S/N]: ").lower().strip()
+        if resposta in ("s", "n"):
             break
 
     lista_jogadores.append(jogador.copy())
     lista_gols.clear()
 
-    if resposta == 'n':
+    if resposta == "n":
         break
 
-print('-='*30)
+print("-=" * 30)
 for indice, jogador in enumerate(lista_jogadores):
     print(f'{indice} | {jogador["nome"]} | {jogador["gols"]} | {jogador["total_gols"]}')
 
 while True:
-    print('-='*30)
-    ver_jogador = int(input('Mostrar dados de qual jogador [999 interrompe o programa]: '))
-        
+    print("-=" * 30)
+    ver_jogador = int(
+        input("Mostrar dados de qual jogador [999 interrompe o programa]: ")
+    )
+
     if ver_jogador == 999:
         break
-        
+
     if ver_jogador > len(lista_jogadores):
-        print('Não existe jogador com esse id.')
+        print("Não existe jogador com esse id.")
     else:
         print(f'\nLevantamento do jogador {lista_jogadores[ver_jogador]["nome"]}:')
-    
+
         for indice in range(len(lista_jogadores[ver_jogador]["gols"])):
-            print(f'Na {indice + 1}ª partida fez {lista_jogadores[ver_jogador]["gols"][indice]}')
+            print(
+                f'Na {indice + 1}ª partida fez {lista_jogadores[ver_jogador]["gols"][indice]}'
+            )
